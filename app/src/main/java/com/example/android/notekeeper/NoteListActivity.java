ef44a188c56910ca3f188f777825803f809acc7c
komalsingh1 <komal17354@gmail.com>
2018-12-29 17:23:57 +0530

    Populated the spinner

---
+++ app/src/main/java/com/example/android/notekeeper/NoteListActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.notekeeper;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+class NodeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_note_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+
+    }
+
+}
